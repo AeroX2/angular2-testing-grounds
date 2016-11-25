60432376ee4df8c59f6e51010d1fef55d32db924
AeroX2 <james@ridey.email>
2016-11-25 19:58:09 +1100

    Finally got testing to work

--- src/app/change-me/change-me.spec.ts
+++ src/app/change-me/change-me.spec.ts
@@ -1,35 +1,53 @@
-import {it, describe, expect, inject, injectAsync, beforeEach, beforeEachProviders} from 'angular2/testing';
+import { TestBed, inject, async } from '@angular/core/testing';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { User } from './user';
-
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Http } from '@angular/http';
-
 import { ChangeMeService } from './change-me.service';
 import { ChangeMeComponent } from './change-me.component';
 
 describe('Change Me Tests', () => {
     let component: ChangeMeComponent;
-    let service: ChangeMeService;
     let userForm: FormGroup;
     let randomForm: FormGroup;
 
-    beforeEach(inject([Http], (http: Http) => {
-        service = new ChangeMeService(http);
-        component = new ChangeMeComponent(service, new FormBuilder());
-        userForm = component.userForm;
-        randomForm = component.randomForm;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ ChangeMeComponent ],
+            imports: [
+                HttpModule,
+            ],
+            providers: [
+                ChangeMeService,
+                FormBuilder,
+            ]
+        });
+
+        TestBed.overrideComponent(ChangeMeComponent, {
+            set: {
+                template: '<div>Overridden template here</div>',
+                templateUrl: ''
+            }
+        });
+    });
+
+    it('Testing User Form', async(() => {
+        TestBed.compileComponents().then(() => {
+
+            component = TestBed.createComponent(ChangeMeComponent).componentInstance;
+            component.ngOnInit();
+
+            userForm = component.userForm;
+            randomForm = component.randomForm;
+
+            userForm.controls['firstName'].setValue("Lucille");
+            userForm.controls['lastName'].setValue("Smith");
+            component.submitForm(userForm.value, userForm.valid);
+
+            expect(userForm.valid).toBe(true);
+            expect(component.users).toContain({firstName: "Lucille", lastName: "Smith"});
+        })
     }));
-
-    it('Testing User Form', () => {
-        userForm.controls['firstName'].setValue("Lucille");
-        userForm.controls['lastName'].setValue("Smith");
-        component.submitForm(userForm.value, userForm.valid);
-
-        expect(userForm.valid).toBe(true);
-        expect(component.users).toBeArray();
-        expect(component.users).toContain({firstName: "Lucille", lastName: "Smith"});
-    })
 
     it('Testing User Form 2', () => {
         userForm.controls['firstName'].setValue("James");
@@ -37,7 +55,6 @@ describe('Change Me Tests', () => {
         component.submitForm(userForm.value, userForm.valid);
 
         expect(userForm.valid).toBe(true);
-        expect(component.users).toBeArray();
         expect(component.users).toContain({firstName: "Lucille", lastName: "Smith"});
         expect(component.users).toContain({firstName: "James", lastName: "Smith"});
     })
@@ -48,7 +65,6 @@ describe('Change Me Tests', () => {
         component.submitForm(userForm.value, userForm.valid);
 
         expect(userForm.valid).toBe(true);
-        expect(component.users).toBeArray();
         expect(component.users.length).toBe(2);
     })
 
@@ -66,18 +82,22 @@ describe('Change Me Tests', () => {
         randomForm.controls['amount'].setValue(5);
         component.getRandomNumber(randomForm.value, randomForm.valid);
 
-        expect(component.random.value).toBeArrayOfNumbers();
-        expect(component.random.value).toBeArrayOfSize(5);
+        for (let num of component.random.value) {
+            expect(typeof num == "number").toBe(true);
+        }
+        expect(component.random.value.length).toBe(5);
     })
 
-    it('Test Random Form Real', () => {
+    it('Test Random Form Real', async(() => {
         randomForm.controls['minNum'].setValue(2);
         randomForm.controls['maxNum'].setValue(8);
         randomForm.controls['amount'].setValue(5);
         randomForm.controls['real'].setValue(true);
-        component.getRandomNumber(randomForm.value, randomForm.valid);
+        component.getRandomNumber(randomForm.value, randomForm.valid)
 
-        expect(component.random.value).toBeArrayOfNumbers();
-        expect(component.random.value).toBeArrayOfSize(5);
-    })
+        expect(component.random.value.length).toBe(5);
+        for (let num of component.random.value) {
+            expect(typeof num == "number").toBe(true);
+        }
+    }))
 })
